fc7250a3680c5fd1ed0421ff68cf3205d90cd277
pit <pit@pit-Aspire-5742G.(none)>
2013-11-10 14:58:16 +0100

    Busy screen - files not included in previous commit

---
+++ public/js/app/appStatus.js
@@ -0,0 +1,21 @@
+define([],function () {
+	return {
+		
+		READY : "ready",
+		UPLOADING_PHOTOS : "transferring photos...",
+		OPENING_CHANNEL : "opening channel...",
+		WAITING_FOR_USERS : "waiting for others to join",
+		JOINING_ROOM : "joining room...",
+		LOADING_PHOTO : "loading photo",
+		isValid : function(status){
+			var keys = Object.keys(this);
+			for (i in keys){
+				var key = keys[i];
+				var val = this[key];
+				if (val == status)
+					return true;
+			};
+			return false;
+		}
+	};
+});
---
+++ public/js/webrtc/roomStatus.js
@@ -0,0 +1,8 @@
+define([],function () {
+	return {
+		
+		NEW_ROOM: 1,
+		JOINING: 2,
+		JOINED: 3
+	};
+});
