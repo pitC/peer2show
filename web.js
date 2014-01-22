34f87d37b3242ca89154863f9d0c3443a3c555a4
pit <pit@pit-Aspire-5742G.(none)>
2014-01-22 22:36:26 +0100

    front page styling

--- web.js
+++ web.js
@@ -15,6 +15,7 @@ app.configure(function(){
  app.use("/js", express.static(__dirname + '/public/js'));
  app.use("/css", express.static(__dirname + '/public/css'));
  app.use("/fonts", express.static(__dirname + '/public/fonts'));
+ app.use("/img", express.static(__dirname + '/public/img'));
  app.all('/', function(req, res){
 	 	res.sendfile(INDEX);
 	 });
