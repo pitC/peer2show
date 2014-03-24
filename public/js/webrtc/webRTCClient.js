f810a4bfef64348e2e8774c6569a75c4a91ef2e8
pit <pit@pit-Aspire-5742G.(none)>
2014-03-24 22:17:04 +0100

    Turn servers commented out.

--- public/js/webrtc/webRTCClient.js
+++ public/js/webrtc/webRTCClient.js
@@ -16,17 +16,17 @@
 //			path: '/myapp',
 			config: {'iceServers': [
 			                        { url: 'stun:stun.l.google.com:19302' }
-			                        ,
-			                        {
-			                            'url': 'turn:192.158.29.39:3478?transport=udp',
-			                            'credential': 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-			                            'username': '28224511:1379330808'
-			                          },
-			                          {
-			                            'url': 'turn:192.158.29.39:3478?transport=tcp',
-			                            'credential': 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-			                            'username': '28224511:1379330808'
-			                          }
+//			                        ,
+//			                        {
+//			                            'url': 'turn:192.158.29.39:3478?transport=udp',
+//			                            'credential': 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+//			                            'username': '28224511:1379330808'
+//			                          },
+//			                          {
+//			                            'url': 'turn:192.158.29.39:3478?transport=tcp',
+//			                            'credential': 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+//			                            'username': '28224511:1379330808'
+//			                          }
 			                      ]}
 		};
 		
