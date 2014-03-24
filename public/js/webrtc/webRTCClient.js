3a756290ca053a816f108c721ce06047b8c0a722
pit <pit@pit-Aspire-5742G.(none)>
2014-03-24 21:48:16 +0100

    trying to fix connection establishing.. Right now working only on localhost

--- public/js/webrtc/webRTCClient.js
+++ public/js/webrtc/webRTCClient.js
@@ -16,7 +16,7 @@
 //			path: '/myapp',
 			config: {'iceServers': [
 			                        { url: 'stun:stun.l.google.com:19302' }
-//			                        ,{ url: 'turn:homeo@turn.bistri.com:80', credential: 'homeo' }
+			                        ,{ url: 'turn:homeo@turn.bistri.com:80', credential: 'homeo' }
 			                      ]}
 		};
 		
