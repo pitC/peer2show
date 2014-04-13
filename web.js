d8ea2d0bca460ae7c0cb73719b1558a290f88d5f
pit <pit@pit-Aspire-5742G.(none)>
2014-04-13 20:12:42 +0200

    Migrated Express 3.x to 4.x. Commited dependencies Tag fot blitz.io added back

--- web.js
+++ web.js
@@ -1,5 +1,4 @@
 var INDEX = "public/index.html";
-var DEMO = "html/All-in-One.html";
 var MAX_ROOM_SIZE = 1;
 var LOCAL_PORT = 8080;
 var static = require('node-static');
@@ -14,11 +13,8 @@ var express = require('express');
 var app = express();
 
 var bugReporter = require('./backend/bugReporter');
+//app.use(express.bodyParser());
 
-app.configure(function(){
-	  app.use(express.bodyParser());
-	  app.use(app.router);
-});
  app.use("/js", express.static(__dirname + '/public/js'));
  app.use("/css", express.static(__dirname + '/public/css'));
  app.use("/fonts", express.static(__dirname + '/public/fonts'));
@@ -27,10 +23,9 @@ app.configure(function(){
  app.all('/', function(req, res){
 	 	res.sendfile(INDEX);
 	 });
- app.all('/demo', function(req, res){
-	 	res.sendfile(DEMO);
-	 });
  
  app.post('/issues', bugReporter.addIssue);
 
  app.listen(process.env.PORT || LOCAL_PORT);
+ 
+ console.log("App started!");
