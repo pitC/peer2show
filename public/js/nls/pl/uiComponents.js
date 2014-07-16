1654fe0592c073703173d337808a8a0200c74352
pit <pit@pit-Aspire-5742G.(none)>
2014-07-16 23:26:40 +0200

    New Session modal translated

---
+++ public/js/nls/pl/uiComponents.js
@@ -0,0 +1,9 @@
+define({
+    // LABELS anywhere
+	
+    "newSessionLbl":"Nowa sesja",
+    "usernameLbl":"Nazwa",
+    // BUTTON labels
+    "createBtn": "Utwórz!"
+    	
+});
---
+++ public/js/nls/uiComponents.js
@@ -0,0 +1,13 @@
+define({
+    "root": {
+        // LABELS anywhere
+    	
+        "newSessionLbl":"New Session",
+        "usernameLbl":"Username",
+        
+        // BUTTON labels
+        "createBtn": "Create"
+        
+    },
+    "pl":true
+});
