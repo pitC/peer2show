8b4f3035923246be5488b60f7015ae0443f256c5
pit <pit@pit-Aspire-5742G.(none)>
2014-08-18 20:30:12 +0200

    Modals localised

--- public/js/nls/uiComponents.js
+++ public/js/nls/uiComponents.js
@@ -1,13 +1,22 @@
 define({
     "root": {
         // LABELS anywhere
-    	
         "newSessionLbl":"New Session",
         "usernameLbl":"Username",
+        "closeSessionLbl":"Close Session",
+        "shareLinkLbl":"Share this link!",
+        "dropFilesLbl":"Drop photos here!",
+        "inviteLbl":"invite some friends!",
         
         // BUTTON labels
-        "createBtn": "Create"
+        "createBtn": "Create",
+        "cancelBtn":"Cancel",
+        "closeBtn":"Close",
         
+        // MESSAGES
+        "closeSessionConfirmMsg":"Are you sure you want to close this session?"
+        
+        	
     },
     "pl":true
 });
