40f6cf1a69cf776c0d9a6f5c1a0d5b0609685d11
pit <pit@pit-Aspire-5742G.(none)>
2014-08-27 22:07:31 +0200

    Initial commit for grunt.js

---
+++ Gruntfile.js
@@ -0,0 +1,10 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json')
+    });
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.registerTask('default', [ 'concat:css', 'cssmin:css', 'concat:js', 'uglify:js' ]);
+};
