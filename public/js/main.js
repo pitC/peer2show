59e5475dfa6ad96ab096d909de3077949c7148e6
pit <pit@pit-Aspire-5742G.(none)>
2014-08-31 14:22:47 +0200

    Removed fallbacks as they're loaded twice

--- public/js/main.js
+++ public/js/main.js
@@ -7,22 +7,23 @@ require.config({
 	baseUrl: 'js',
 	paths: {
 		
-		jquery: ['http://code.jquery.com/jquery-1.10.1.min', 
-		         'lib/fallback/jquery-1.10.1.min'
+		jquery: ['http://code.jquery.com/jquery-1.10.1.min'
+		         //, 'lib/fallback/jquery-1.10.1.min'
 		         ],
 		jqueryui: [
-		           'http://code.jquery.com/ui/1.10.3/jquery-ui',
-		           'lib/fallback/jquery-ui'
+		           'http://code.jquery.com/ui/1.10.3/jquery-ui'
+//		           ,'lib/fallback/jquery-ui'
 		           ],
 		underscore: [
-		             'http://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.4.4/underscore-min',
-		             'lib/fallback/underscore-min' 
+		             'http://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.4.4/underscore-min'
+//		             ,'lib/fallback/underscore-min' 
 		             ],
-		backbone: ['http://cdnjs.cloudflare.com/ajax/libs/backbone.js/1.0.0/backbone-min',
-		           'lib/fallback/backbone-min'],
+		backbone: ['http://cdnjs.cloudflare.com/ajax/libs/backbone.js/1.0.0/backbone-min'
+//		           ,'lib/fallback/backbone-min'
+		           ],
 		bootstrap: [
-		            'http://netdna.bootstrapcdn.com/bootstrap/3.0.2/js/bootstrap.min',
-		            'lib/fallback/bootstrap.min'
+		            'http://netdna.bootstrapcdn.com/bootstrap/3.0.2/js/bootstrap.min'
+//		            ,'lib/fallback/bootstrap.min'
 		            ],
 		tiny_color : 'lib/tinycolor-0.9.15.min',
 		jquery_pick_a_color: 'lib/pick-a-color-1.1.7.min',
