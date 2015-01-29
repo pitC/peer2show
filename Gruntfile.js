c28365dc724efaff6ff6f916ee201d8537791890
pit <pit@pit-Aspire-5742G.(none)>
2015-01-29 22:19:42 +0100

    .

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,56 +1,18 @@
 module.exports = function(grunt) {
-//    grunt.initConfig({
-////    	clean: {
-////    	      tests: ['tmp'],
-////    	    },
-////    	concat: {
-////            css: {
-////               src: [
-////                     'public/css/*'
-////                    ],
-////                dest: 'public/combined.css'
-////            },
-////            js : {
-////            	
-////            }
-////        },
-////        cssmin : {
-////            css:{
-////                src: 'public/combined.css',
-////                dest: 'public/combined.min.css'
-////            }
-////        },
-//        uglify : {
-//        	build: {
-//                src: 'public/js/app.js',
-//                dest: 'public/js/app.js'
-//            }
-//        },
-////        buildcontrol: {
-////            options: {
-////              dir: 'dist',
-////              commit: true,
-////              push: true,
-////              message: 'Built %sourceName% from commit %sourceCommit% on branch %sourceBranch%'
-////            },
-////            development: {
-////              options: {
-////                remote: 'staging',
-////                branch: 'master'
-////              }
-////            },
-////            
-////        },
-////        copy: {
-////      	  main: {
-////      	    src: '*',
-////      	    dest: 'dist/',
-////      	  },
-////      	},
-//        pkg: grunt.file.readJSON('package.json')
+	  grunt.initConfig({
+		  uglify: {
+		     
+			  files: { 
+	               src: 'js/**/*.js',  // source files mask
+	               dest: './',    // destination folder
+	               expand: true,    // allow dynamic building
+	               flatten: true,   // remove all unnecessary nesting
+	           }
+		    }
+	  });
 //    });
 //    grunt.loadNpmTasks('grunt-contrib-concat');
-//    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
 //    grunt.loadNpmTasks('grunt-contrib-watch');
 //    grunt.loadNpmTasks('grunt-contrib-cssmin');
 //    grunt.loadNpmTasks('grunt-build-control');
