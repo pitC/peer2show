ce2158dacd324fccf32d8bf68c687542088b6666
pit <pit@pit-Aspire-5742G.(none)>
2015-02-01 09:13:53 +0100

    Grunt working. Uglify css and js files

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,29 +1,24 @@
 module.exports = function(grunt) {
-	  grunt.initConfig({
-		  uglify: {
+	grunt.initConfig({
+		uglify: {
 		     
 			  files: { 
-				  cwd:"public/js/",
+				   cwd:"public/js/",
 	               src: '**/*.js',  // source files mask
 	               dest: 'public/js/',    // destination folder
 	               expand: true,    // allow dynamic building
 	               //flatten: true,   // remove all unnecessary nesting
 	           }
-		    }
+		    },
+		    cssmin:{
+		    	css:{
+	                src: 'public/css/style.css',
+	                dest: 'public/css/style.css'
+	            }
+	  		}
 	  });
-//    });
-//    grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
-//    grunt.loadNpmTasks('grunt-contrib-watch');
-//    grunt.loadNpmTasks('grunt-contrib-cssmin');
-//    grunt.loadNpmTasks('grunt-build-control');
-//    grunt.loadNpmTasks('grunt-contrib-copy');
-//    grunt.registerTask('default', [ 'concat:css', 'cssmin:css', 'concat:js', 'uglify:build',"copy:main" ]);
-//    
-//    grunt.registerTask('build', [ 'buildcontrol:development'
-//                                 // Collection of tasks that build code to the 'dist' directory...
-//                               ]);
-////    grunt.registerTask('heroku:production', 'clean mincss uglify');
-	grunt.registerTask('heroku:production', 'uglify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.registerTask('heroku:production', ['uglify','cssmin:css']);
     
 };
