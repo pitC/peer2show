bb6f819e903c80a71030bc931a79e0e9916d6cf0
pitC <p.k.cabaj@gmail.com>
2018-10-16 22:39:43 +0200

    LOCAL DB changed do MONGOHQ

--- backend/db/db.js
+++ backend/db/db.js
@@ -2,7 +2,7 @@
  * New node file
  */
 
-var LOCAL = 'mongodb://heroku:pPXbFCuYc4C5LsEV61bx-Ot7DLQY2pjrumPYYkv9aqm0dOg3MbrDCUT_qkLlv8LFtBbWbC2Ly7t_t2a7ZdKP5g@linus.mongohq.com:10093/app28849408';
+var LOCAL = 'mongodb://heroku_3z1rgh5v:ch5cjnla9sb2pgak967f1o23s2@ds151702.mlab.com:51702/heroku_3z1rgh5v'
 
 module.exports = {
 	  'url' : process.env.MONGOHQ_URL||LOCAL
