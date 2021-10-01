81544ff20e5c673bae41a51d4a96df212373e2c2
Piotr Cabaj <p.k.cabaj@gmail.com>
2021-10-01 20:36:47 +0100

    Update db.js

--- backend/db/db.js
+++ backend/db/db.js
@@ -2,7 +2,7 @@
  * New node file
  */
 
-var LOCAL = 'mongodb://heroku_3z1rgh5v:ch5cjnla9sb2pgak967f1o23s2@ds151702.mlab.com:51702/heroku_3z1rgh5v'
+var LOCAL = 'mongodb://localhost:51702/peershow'
 
 module.exports = {
 	  'url' : process.env.MONGOHQ_URL||LOCAL
